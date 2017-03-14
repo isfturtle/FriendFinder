ea503fe70d12fd4f94aa7f8cc4c50113d75d58ff
Ilona Fleischer <Ilona Fleischer>
2017-03-13 23:58:22 -0400

    I can't get the photo to show up. Everything else works.

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -8,6 +8,7 @@ app.get("/api/friends", function(req, res){
 
 app.post("/api/friends", function(req, res){
 	newFriend = req.body;
+	console.log(newFriend);
 	if(friendsArray.length===0){
 		friendsArray.push(newFriend);
 	}
@@ -17,15 +18,17 @@ app.post("/api/friends", function(req, res){
 		for(var i = 0; i<friendsArray.length;i++){
 			var matchScore = 0;
 			for(var j=0; j<10;j++){
-				matchScore+=Math.abs(friendsArray[i].score[j]-newFriend.score[j]);
+				matchScore+=Math.abs(friendsArray[i].scores[j]-newFriend.scores[j]);
 			}
 			if(matchScore<bestMatchScore){
 				bestMatch=friendsArray[i];
 				bestMatchScore=matchScore;
 			}
 		}
+		friendsArray.push(newFriend);
 		res.json(bestMatch);
 	}
+	console.log(friendsArray);
 });
 
 
